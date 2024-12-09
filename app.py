125ac9c344182659dab1202713638796031cdff6
saglearn <100061966+saglearn@users.noreply.github.com>
2024-12-09 15:24:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,51 @@ import os
 from PIL import Image
 import google.generativeai as genai
 
-
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## Function to load gemini pro vision
 model=genai.GenerativeModel('gemini-pro-vision')
 
 def get_gemini_response(input, image, prompt):
-    response
+    response=model.generate_content([input, image[0], prompt])
+    return response.text
 
+def input_image_details(uploaded_file):
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts= [
+            {
+                "mime_type": uploaded_file.type, # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="MultiLanguage Invoice Extractor")
+
+st.header("MultiLanguage Invoice Extractor")
+input=st.text_input("Input Prompt:  ", key="input")
+uploaded_file= st.file_uploader("Choose an image of the invoice... ", type=["jpg", "jpeg", "png"])
+image=""
+if uploaded_file is not None:
+    image=Image.open(uploaded_file)
+    st.image(image, caption="Upload Image.", use_column_width=True)
+
+submit=st.button("Tell me about the invoice")
+
+input_prompt="""
+You are an expert in understading invoices. We will upload a image as invoice and you will have to anwser any questions based on the uploaded image
+"""
+
+# If submit button is clicked
+if submit:
+    image_data=input_image_details(uploaded_file)
+    response=get_gemini_respose(input_prompt, image_data, input)
+    st.subheader("The Response is")
+    st.write(response)
