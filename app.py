28b52ba41d4b261ebe064285a1388ff984786eeb
saglearn <100061966+saglearn@users.noreply.github.com>
2024-12-09 14:54:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv #install python .env to use this
 
 load_dotenv() ##load all the environment variables from .env
+
+import streamlit as st
+import os
+from PIL import Image
+import google.generativeai as genai
+
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load gemini pro vision
+model=genai.GenerativeModel('gemini-pro-vision')
+
+def get_gemini_response(input, image, prompt):
+    response
 
